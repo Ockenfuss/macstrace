db79996604c59754e3e67482d88bea22fe6939e6
Paul Ockenfuss <paul.ockenfuss@campus.lmu.de>
2020-06-24 17:57:33 +0200

    Rewritten Version of Transform -Halo does now contain the two sensors. This mirrors reality better. -Halo object does cache the transformations it got from the mounttree.

--- macstrace/Transform.py
+++ macstrace/Transform.py
@@ -9,24 +9,15 @@ import mounttree
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 import unittest as ut
+from collections import defaultdict
 #%%
-class Intersector(object):
-    def intersect(self,px, py, pz,vx, vy,vz):
-        raise(NotImplementedError)
-
-class Cloud_intersector(Intersector):
-    def __init__(self, height=2000):
-        self.h=-height
-    def intersect(self,px, py, pz,vx, vy,vz):
-        """Coordinates in NED with origin at the surface"""
-        t=(self.h-pz)/vz
-        return px+t*vx, py+t*vy
 
 class Halo(object):
-    def __init__(self, coord_universe,orientation):
+    def __init__(self, coord_universe,orientation, sensor1, sensor2):
         self.coord_universe=coord_universe
         self.orientation=orientation.sortby('time')
-        self.sensor='vnir'
+        self.sensors={'sensor1':sensor1, 'sensor2':sensor2}
+        self.transform_cache={'sensor1':{}, 'sensor2':{}}
         self.reference_available=False
     
     def _add_reference_frame(self, time):
@@ -38,38 +29,58 @@ class Halo(object):
         self.reference_available=True
 
     @classmethod
-    def from_files(cls, mounttreefile, bahamasfile):
+    def from_files(cls, mounttreefile, bahamasfile, sensor1file, sensor2file):
         geometry=xr.open_dataset(bahamasfile)
         coord_universe=mounttree.load_mounttree(mounttreefile)
-        return cls(coord_universe, geometry)
+        return cls(coord_universe, geometry, Sensor.from_file(sensor1file), Sensor.from_file(sensor2file))
     
-    def get_sensor_to_reference_transform(self, time):
+    @classmethod
+    def from_datasets(cls, mounttreefile, bahamasfile, data1, data2):
+        geometry=xr.open_dataset(bahamasfile)
+        coord_universe=mounttree.load_mounttree(mounttreefile)
+        return cls(coord_universe, geometry, Sensor.from_dataset(data1), Sensor.from_dataset(data2))
+
+    def _create_sensor_to_reference_transform(self,sensor, time):
         if not self.reference_available:
             self._add_reference_frame(time)
         keys=['lat', 'lon', 'height', 'yaw', 'pitch', 'roll']
         current_orientation={key:self.orientation[key].interp(time=time).values for key in keys}
-        transformation=self.coord_universe.get_transformation(self.sensor.upper(), 'intersection_reference', **current_orientation)
+        transformation=self.coord_universe.get_transformation(self.sensors[sensor].name.upper(), 'intersection_reference', **current_orientation)
         return transformation
+    
+    def get_transform(self, sensor, time):
+        try:
+            trans=self.transform_cache[sensor][time]
+        except KeyError:
+            trans=self._create_sensor_to_reference_transform(sensor, time)
+            self.transform_cache[sensor][time]=trans
+        return trans
     
     def altact_to_sensor(self,alt, act):
         """Convert along and across track angle to dir. vectors. Input in degrees!!!."""
         return [np.sin(deg2rad(act)), np.sin(deg2rad(alt))*np.cos(deg2rad(act)), np.cos(deg2rad(alt))*np.cos(deg2rad(act))]
     
-    def _get_abs_view_direction_single(self, alt,act, time):
-        transform=self.get_sensor_to_reference_transform(time)
-        direction=transform.apply_direction(*self.altact_to_sensor(alt,act))
-        return np.stack(direction, axis=-1)
-    def _get_abs_view_pos_single(self, time):
-        transform=self.get_sensor_to_reference_transform(time)
-        position=transform.apply_point(0,0,0)
-        return np.stack(position, axis=-1)
+    def _get_abs_view_direction_single(self, sensor, time):
+        transform=self.get_transform(sensor, time)
+        get_direction=lambda alt, act:np.stack(transform.apply_direction(*self.altact_to_sensor(alt,act)), axis=-1)
+        direction=xr.apply_ufunc(get_direction, self.sensors[sensor].alt,self.sensors[sensor].act,output_core_dims=[['coords']])
+        direction.coords['coords']=('coords', ['x', 'y', 'z'])
+        direction=direction.expand_dims({'time':[time]})
+        return direction
 
-    def get_abs_view_direction(self, alt, act, times):
-        direction=[self._get_abs_view_direction_single(alt, act, t) for t in times]
-        return np.stack(direction, axis=-1)
-    def get_abs_view_position(self, times):
-        pos=[self._get_abs_view_pos_single(t) for t in times]
-        return np.stack(pos,axis=-1)
+    def _get_abs_view_pos_single(self,sensor, time):
+        transform=self.get_transform(sensor, time)
+        position=xr.DataArray(np.stack(transform.apply_point(0,0,0), axis=-1),coords=[('coords', ['x', 'y', 'z'])])
+        position=position.expand_dims({'time':[time]})
+        return position
+
+    def get_abs_view_direction(self, sensor):
+        direction=xr.concat([self._get_abs_view_direction_single(sensor, time) for time in self.sensors[sensor].times.values], dim='time')
+        return direction
+
+    def get_abs_view_position(self, sensor):
+        position=xr.concat([self._get_abs_view_pos_single(sensor, time) for time in self.sensors[sensor].times.values], dim='time')
+        return position
 
 class Nearest_point(object):
     def __init__(self, fitx, fity):
@@ -78,7 +89,8 @@ class Nearest_point(object):
         self.fitcoords=fitx.coords
         self.fitshape=fitx.shape
         self.fitdims=fitx.dims
-        self.nbs=NearestNeighbors(n_neighbors=1, algorithm='auto').fit(self._flatten_combinexy(fitx.values,fity.values))
+        print("Building up kdtree...")
+        self.nbs=NearestNeighbors(n_neighbors=1, algorithm='kd_tree', leaf_size=100).fit(self._flatten_combinexy(fitx.values,fity.values))
     
     def _flatten_combinexy(self,x,y):
         return np.column_stack((x.flatten(),y.flatten()))
@@ -93,6 +105,7 @@ class Nearest_point(object):
 
 
     def nearest_index(self,x,y):
+        print("Evaluating using kdtree...")
         index=xr.apply_ufunc(self._predict_array,x,y)
         index=xr.apply_ufunc(self._unravel_fitindex, index, output_core_dims=[['fitdim']])
         index.coords['fitdim']=('fitdim', list(self.fitdims))
@@ -114,29 +127,26 @@ class Sensor(object):
         return cls(data.alt, data.act, data.time, data.source_name)
 
 class Transformer(object):
-    def __init__(self,sensor1, sensor2, halo):
-        self.sensor1=sensor1
-        self.sensor2=sensor2
+    def __init__(self,halo, intersector):
         self.halo=halo
-        self.intersector=None
+        self.intersector=intersector
 
     @classmethod
-    def from_files(cls,mounttreefile, orientationfile, sensor1file, sensor2file):
-        halo=Halo.from_files(mounttreefile, orientationfile)
-        return cls(Sensor.from_file(sensor1file), Sensor.from_file(sensor2file), halo)
+    def from_files(cls,mounttreefile, orientationfile, sensor1file, sensor2file, intersector):
+        halo=Halo.from_files(mounttreefile, orientationfile, sensor1file, sensor2file)
+        return cls(halo, intersector)
         
     @classmethod
-    def from_datasets(cls, mounttreefile, orientationfile, data1, data2):
-        halo=Halo.from_files(mounttreefile, orientationfile)
-        return cls(Sensor.from_dataset(data1), Sensor.from_dataset(data2), halo)
+    def from_datasets(cls, mounttreefile, orientationfile, data1, data2, intersector):
+        halo=Halo.from_datasets(mounttreefile, orientationfile, data1, data2)
+        return cls(halo, intersector)
 
     def get_points_on_cloud(self, sensor):
-        assert(self.intersector is not None)
-        self.halo.sensor=sensor.name
-        directions=xr.apply_ufunc(self.halo.get_abs_view_direction,sensor.alt,sensor.act,sensor.times,input_core_dims=[[],[],['time']],output_core_dims=[['coords','time']])
-        directions.coords['coords']=('coords', ['x','y', 'z'])
-        positions=xr.apply_ufunc(self.halo.get_abs_view_position,sensor.times,input_core_dims=[['time']],output_core_dims=[['coords','time']])
-        positions.coords['coords']=('coords', ['x','y', 'z'])
+        print(f"Calculating directions for {sensor}...")
+        directions=self.halo.get_abs_view_direction(sensor)
+        print(f"Calculating positions for {sensor}...")
+        positions=self.halo.get_abs_view_position(sensor)
+        print(f"Calculating intersections for {sensor}...")
         x,y=self.intersector.intersect(positions.sel(coords='x'),positions.sel(coords='y'),positions.sel(coords='z'),directions.sel(coords='x'),directions.sel(coords='y'),directions.sel(coords='z'))
         return x,y
 
@@ -145,8 +155,8 @@ class Transformer(object):
         return xr.DataArray(result,coords=next(iter(kwargs.values())).coords)
 
     def transform(self, data):
-        kdtree=Nearest_point(*self.get_points_on_cloud(self.sensor1))
-        index=kdtree.nearest_index(*self.get_points_on_cloud(self.sensor2))
+        kdtree=Nearest_point(*self.get_points_on_cloud('sensor1'))
+        index=kdtree.nearest_index(*self.get_points_on_cloud('sensor2'))
         result=self._isel_coord_multiindex(data, **{key:index.sel(fitdim=key) for key in data.dims})
         return result.drop_vars('fitdim')
 
